bb3dc83bb8d5351736bde87fefa4e5e0c0e5db38
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2024-09-24 15:29:55 +0100

    Revert actions param

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@ import "../src/index.css";
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
